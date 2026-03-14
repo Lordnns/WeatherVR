b05575ced1670eb856f2ca45888890bda73b4606
lordnns <Samuelmgrise@gmail.com>
2026-03-14 17:13:39 -0400

    Fix input Field in build

---
+++ WeatherVR/Assets/Scripts/QuestKeyboardTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class QuestKeyboardTrigger : MonoBehaviour
+{
+    private TMP_InputField _inputField;
+
+    private void Awake()
+    {
+        _inputField = GetComponent<TMP_InputField>();
+        _inputField.onSelect.AddListener(OnSelected);
+    }
+
+    private void OnSelected(string value)
+    {
+        TouchScreenKeyboard.Open(value, TouchScreenKeyboardType.Default);
+    }
+}
